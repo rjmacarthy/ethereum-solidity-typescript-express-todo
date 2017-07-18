47824b7efee448385f98a22cbd91bfb7fad95def
rjmacarthy <richardmacarthy@hotmail.com>
2017-07-18 14:19:07 +0100

    Debugger remove

--- server/src/services/web3.server.service.ts
+++ server/src/services/web3.server.service.ts
@@ -1,7 +1,6 @@
 import * as Web3 from 'web3';
 import * as abi from '../../../build/contracts/TodoContract.json';
 import * as contract from 'truffle-contract';
-debugger;
 const provider = new Web3.providers.HttpProvider("http://localhost:8545");
 const web3 = new Web3(provider);
 const todoContract = contract(abi);
